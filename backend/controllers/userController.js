4df2cdabdf46eb3d3aa241da3c803fb5c2c7f165
Kiran M <kiran7619401943@gmail.com>
2026-02-16 12:36:26 +0530

    changed backend content for testing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,7 @@ exports.registerUser = async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ message: "User registered success" });
+    res.status(201).json({ message: "User registered successfully" });
 
   } catch (error) {
     console.error(error);
